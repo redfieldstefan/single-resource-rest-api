912e7dcff2bc6e2f013f36d5ad40a6e2be8882d0
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-08 13:34:58 -0700

    add grunt task mgmt

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-simple-mocha');
+
+	var srcFiles = ['Gruntfile.js', './test/**/*test.js', './routes/*.js', './models/*.js'];
+	
+
+	grunt.initConfig({
+
+		jshint: {
+			dev: {
+				src: srcFiles
+			},
+
+			options: {
+				jshintrc: true
+			}
+		},
+
+		simplemocha: {
+			dev: {
+				src: ['./test/**/*test.js']
+			}
+		},
+
+		watch: {
+			files: srcFiles,
+			tasks: ['jshint:dev']
+		}
+
+	});
+
+	grunt.registerTask('test', ['jshint:dev', 'simplemocha:dev']);
+	grunt.registerTask('default', ['test']);
+};
+
