e46d944d19f246d1190f1f802e839c69dec711d7
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-10 13:06:56 -0700

    add custom validators

--- models/Rant.js
+++ models/Rant.js
@@ -1,11 +1,27 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var validate = require('mongoose-validator');
+
+var titleValidator = [
+	validate({
+		validator: 'isLength',
+		arguments: [1, 15],
+		message: 'Rant title must be under 15 characters'
+	})
+];
+
+var bodyValidator = [
+	validate({
+		validator: 'isLength',
+		arguments: [1, 50],
+		message: 'Rant must be under 50 characters'
+	})
+];
 
 var rantSchema = mongoose.Schema({
-	title: String,
-	author: String,
-	rant: String,
+	title: {type: String, required: 'title is required', validate: titleValidator},
+	rant: {type: String, required: 'a rant is required', validate: bodyValidator},
 	recipient: String
 });
 
