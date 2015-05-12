95d69bd579de19e5c152cf18ceb8f039310736b5
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-12 12:43:40 -0700

    update to a sql model

--- models/Rant.js
+++ models/Rant.js
@@ -6,43 +6,12 @@ var sql = new Sql('notes_dev', 'notes_dev', 'foobar123', {
 });
 
 var Rant = module.exports = sql.define('Rant', {
-	rantBody: Sql.STRING
+	rantBody: Sql.STRING,
+	title: Sql.STRING,
+	rant: Sql.STRING
 });
+
+
 
 Rant.sync();
 
-
-
-
-
-
-
-
-// 'use strict';
-
-// var mongoose = require('mongoose');
-// var validate = require('mongoose-validator');
-
-// var titleValidator = [
-// 	validate({
-// 		validator: 'isLength',
-// 		arguments: [1, 15],
-// 		message: 'Rant title must be under 15 characters'
-// 	})
-// ];
-
-// var rantValidator = [
-// 	validate({
-// 		validator: 'isLength',
-// 		arguments: [1, 50],
-// 		message: 'Rant must be under 50 characters'
-// 	})
-// ];
-
-// var rantSchema = mongoose.Schema({
-// 	title: {type: String, required: 'title is required', validate: titleValidator},
-// 	rant: {type: String, required: 'a rant is required', validate: rantValidator},
-// 	recipient: String
-// });
-
-// module.exports = mongoose.model('Rant', rantSchema);
