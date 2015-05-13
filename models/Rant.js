f7975656689e3b41cd74cfde6524690c37e87931
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-12 17:53:36 -0700

    point to procfiles

--- models/Rant.js
+++ models/Rant.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Sql = require('sequelize');
-var sql = new Sql('notes_dev', 'notes_dev', 'foobar123', {
+var sql = new Sql(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASS, {
 	dialect: 'postgres'
 });
 
