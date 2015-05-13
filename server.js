22135253432fca618c385532defcf8e77aed97c6
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-13 15:13:43 -0700

    update to accomodate auth

--- server.js
+++ server.js
@@ -2,17 +2,23 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
-
+var passport = require('passport');
 var app = express();
 var port = 3000;
 
 var rantRoutes = express.Router();
+var userRoutes = express.Router();
 
+process.env.APP_SECRET = process.env.APP_SECRET || 'changethischangethis';
 mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://localhost/rant_store');
 
+app.use(passport.initialize());
+require('./lib/passport_strat')(passport);
+require('./routes/user_routes')(userRoutes, passport);
 require('./routes/rant_routes')(rantRoutes);
 
 app.use('/api', rantRoutes);
+app.use('/api', userRoutes);
 
 app.listen(process.env.port || port, function () {
 	console.log('server running on port ' + (process.env.PORT || port));
