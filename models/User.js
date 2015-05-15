fb05b25427c4c1563a220b2dc76b3d6a5b3909b5
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-15 15:57:12 -0700

    generate hash and check password now async

--- models/User.js
+++ models/User.js
@@ -4,20 +4,35 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var eat = require('eat');
 
-var userSchema = mongoose.Schema({
-	username: {type:String, required: true, trim: true},
+
+var userSchema = new mongoose.Schema({
+	username: {type:String, required: true, unique:true, trim: true},
 	basic: {
 		email:{type: String, required: true, unique: true, trim: true},
-		password:{type: String, min: 7, required: true, trim: true}
+		password: String
 	}
 });
 
-userSchema.methods.generateHash = function(password) {
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+userSchema.methods.generateHash = function(password, callback) {
+	 bcrypt.genSalt(8, function(err, salt) {
+		bcrypt.hash(password, salt, null, function(err, hash) {
+			if (err) {
+				console.log(err);
+			}
+			callback(err, hash);
+		});
+	});
 };
 
-userSchema.methods.checkPassword = function(password) {
-	return bcrypt.compareSync(password, this.basic.password);
+userSchema.methods.checkPassword = function(password, callback) {
+	bcrypt.compare(password, this.basic.password, function(err, res) {
+		console.log(res);
+		if(err) {
+			console.log(err);
+		}
+		callback(err, res);
+	});
 };
 
 userSchema.methods.generateToken = function(secret, callback) {
