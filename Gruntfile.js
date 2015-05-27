cc8a440648acdfdce292cd26122b35f592088938
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-27 09:34:52 -0700

    add webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,48 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-simple-mocha');
 	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-webpack');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	var srcFiles = ['Gruntfile.js', './test/**/*test.js', './routes/*.js', './models/*.js', './lib/*.js'];
-	
 
 	grunt.initConfig({
+
+		webpack: {
+			client: {
+				entry: __dirname + '/app/js/client.js',
+				output: {
+					path:'build/',
+					file:'bundle.js'
+				}
+			},
+
+			test: {
+				entry: __dirname + '/test/client/test.js',
+				output: {
+					path: 'test/client',
+					file: 'test_bundle.js'
+				}
+			}
+		},
+
+		copy:{
+			html:{
+				cwd: 'app/',
+				expand: true,
+				flatten: false,
+				src:'**/*.html',
+				dest:'build/',
+				filter:'isFile'
+			}
+		},
+
+		clean:{
+			dev:{
+				src:'build/'
+			}
+		},
 
 		jshint: {
 			dev: {
@@ -42,6 +79,7 @@ module.exports = function (grunt) {
 	});
 
 	grunt.registerTask('test', ['jshint:dev', 'simplemocha:dev']);
+	grunt.registerTask('build:dev', ['webpack:client', 'copy:html'])
 	grunt.registerTask('default', ['test']);
 };
 
