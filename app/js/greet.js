8c6df9d5be4551ec92dec16453abba04e4132a6b
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-01 13:23:13 -0700

    code clean

--- app/js/greet.js
+++ app/js/greet.js
@@ -2,4 +2,4 @@
 
 module.exports = function(name) {
 	return 'Here we get to view Rants brought to you by ' + name +'. Each rant has a title, a rant, and a recipient. Titles and Rants are required' ;
-}
+};
