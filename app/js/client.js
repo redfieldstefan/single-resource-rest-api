c14995b85ed695f961f29bd620f66b8c5d16f2a4
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-01 00:12:14 -0700

    add cancel function

--- app/js/client.js
+++ app/js/client.js
@@ -1,25 +1,7 @@
 'use strict';
 
-var greet = require('./greet');
-document.write(greet('Stefan Redfield'));
-var request = require('superagent');
-var rantList = document.getElementById('rantList');
-var body = document.body;
+require('angular/angular');
 
- request
-	.get('/api/rants')
-	.end(function (err, res) {
-		if(err) {
-			return console.log(err)
-		}
+var rantsApp = angular.module('rantsApp', []);
 
-		var rants = JSON.parse(res.text);
-		rants.forEach(function(rant) {
-			console.log(rant.rant);
-			var rantEl = document.createElement('li');
-			// var test = document.createTextNode(rant.title + ': ' + rant.rant);
-			rantEl.innerHTML = rant.title + ': ' + rant.rant;
-			// rantEl.appendChild(test);
-			rantList.appendChild(rantEl);
-		});
-	});
+require('../rants/controllers/rants.js')(rantsApp);
---
+++ app/rants/controllers/rants.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function(app) {
+	app.controller('rantsController', ['$scope', '$http', function($scope, $http) {
+		$scope.errors = [];
+		$scope.rants = [];
+		$scope.getAll = function() {
+			$http.get('/api/rants')
+				.success(function(data){
+					$scope.rants = data;
+				})
+				.error(function(data){
+					console.log(data);
+					$scope.errors.push({msg: 'error retrieving rants'});
+				});
+		};
+		$scope.createNewRant = function() {
+			$http.post('/api/rants', $scope.newRant)
+				.success(function(data) {
+					$scope.rants.push(data);
+					$scope.newRant = null;
+				})
+				.error(function(data) {
+					console.log(data);
+					$scope.errors.push({msg: 'could not create new Rant'});
+				});
+		};
+
+		$scope.reset = function(){
+			$scope.rants.$rollbackViewValue();
+		};
+
+		$scope.removeRant = function(rant) {
+			$scope.rants.splice($scope.rants.indexOf(rant), 1);
+			$http.delete('/api/rants/' + rant._id)
+				.error(function(data) {
+					console.log(data);
+					$scope.errors.push({msg:'could not remove rant: ' + rant.rant});
+				});
+		};
+
+		$scope.saveRant = function(rant) {
+			rant.editing = false;
+			$http.put('/api/rants/' + rant._id, rant)
+				.error(function(data) {
+					console.log(data);
+					$scope.errors.push({msg: 'Could not Save Rant'});
+				});
+
+		};
+	}]);
+}
