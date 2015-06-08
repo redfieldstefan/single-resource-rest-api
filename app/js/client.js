ab561ab82d22d1d8666daf49418a97c7acfda81e
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-08 13:49:06 -0700

    minor updates

--- app/js/client.js
+++ app/js/client.js
@@ -10,6 +10,7 @@ var rantsApp = angular.module('rantsApp', ['ngRoute', 'ngCookies', 'base64']);
 //SERVICES
 require('./services/rest_resource.js')(rantsApp);
 require('./services/copy')(rantsApp);
+// require('./auth/services/auth')(rantsApp);
 
 //CONTROLLERS
 require('./rants/controllers/rants.js')(rantsApp);
@@ -17,15 +18,13 @@ require('./auth/controllers/auth_controller.js')(rantsApp)
 
 //DIRECTIVES
 require('./directives/rant_form_directive.js')(rantsApp);
+// require('./auth/directives/logout_direction')(rantsApp);
 
 rantsApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/rants', {
 			templateUrl: 'templates/views/rants_view.html',
 			controller: 'rantsController'
-		})
-		.when('/', {
-			redirectTo: '/create_user'
 		})
 		.when('/sign_in', {
 			templateUrl: 'templates/views/sign_in_view.html',
